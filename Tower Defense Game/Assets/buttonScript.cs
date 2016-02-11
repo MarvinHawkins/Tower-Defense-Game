70705ba54304163e5fd4509d5e882222c18a634e
MarvinHawkins <m.louis.hawkins@gmail.com>
2016-02-11 07:12:27 -0600

    Bu tt on St uf

---
+++ Tower Defense Game/Assets/buttonScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class buttonScript : MonoBehaviour {
+
+    public GameObject[] Towers; //Create an array of t he towers available to the player
+
+	public void buildTower(int tower){
+
+        GameObject newTower = Instantiate(Towers[tower]);
+
+    }
+}
